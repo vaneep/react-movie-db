38aaaba80ad48f588bb20fd092d71b8a3bfa84e8
Vanessa Elizabeth Piccoli <vanepiccoli7@gmail.com>
2022-09-12 20:13:42 -0300

    Created new pages and routes

--- movies_lib/src/main.jsx
+++ movies_lib/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="/" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
